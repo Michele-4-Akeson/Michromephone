f60aaaa27b8adea256e287d8f10caf3e0a7a5ddf
Michele Akeson <akesonm@mcmaster.ca>
2022-12-19 19:41:39 -0500

    moving project from heroku

--- src/Actions/BackendActions.js
+++ src/Actions/BackendActions.js
@@ -1,6 +1,6 @@
 import {nanoid} from 'nanoid'
 
-const backendURL = "https://michromephone.herokuapp.com/"
+const backendURL = "https://michromephone-api.onrender.com/"
 const twilioPath = "twilio"
 const profilePath = "profile"
 
